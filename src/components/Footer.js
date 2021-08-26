e22488b0e7874d5e04d813670d4382812b95b684
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-25 21:19:16 -0700

    creating a header and footer to display

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer id="footer">
+            <ul>
+                <li><a href="https://www.instagram.com/pranavbyakod/">Social</a></li>
+                <li><a href="mailto: pranavbyakod@gmail.com">Email</a></li>
+                <li><a href="https://github.com/pbyakod">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/pranavbyakod/">Linkedin</a></li>
+            </ul>
+        </footer>
+    )
+}
