07bfe134dfd9eb01a61783cba3a119333d8e7b8e
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-25 22:21:57 -0700

    adding icons to navbar to liven up the page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,20 +8,20 @@ function Navigation ({ currentPage, handlePageChange}) {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="#home"  onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</a>
+                            <a class="nav-link" href="#home"  onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}><i class="fas fa-home"></i> Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#about"  onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
+                            <a class="nav-link" href="#about"  onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}><i class="fas fa-id-card"></i> About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#works"  onClick={() => handlePageChange('Works')} className={currentPage === 'Works' ? 'nav-link active' : 'nav-link'}>Works</a>
+                            <a class="nav-link" href="#works"  onClick={() => handlePageChange('Works')} className={currentPage === 'Works' ? 'nav-link active' : 'nav-link'}><i class="fas fa-folder-open"></i> Works</a>
                         </li>
                     </ul>
                     <li class="d-flex nav-item">
-                        <a class="nav-link" href="#contact"  onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
+                        <a class="nav-link" href="#contact"  onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}><i class="fas fa-address-book"></i> Contact</a>
                     </li>
                     <li class="d-flex nav-item">
-                        <a class="nav-link" href="#resume"  onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
+                        <a class="nav-link" href="#resume"  onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}><i class="fas fa-file-contract"></i> Resume</a>
                     </li>
                 </div>
             </div>
